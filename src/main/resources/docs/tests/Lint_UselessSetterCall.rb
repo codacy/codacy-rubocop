a381527be3b4e4b51573f58e3dde82390c1b2cbd
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-08-24 17:42:35 +0100

    tests the new plugins test method on useless setter call pattern

--- src/main/resources/docs/tests/Lint_UselessSetterCall.rb
+++ src/main/resources/docs/tests/Lint_UselessSetterCall.rb
@@ -1,7 +1,7 @@
 ##Patterns: Lint_UselessSetterCall
+##Issue: {"severity": "Info", "line": 6, "patternId": "Lint_UselessSetterCall"}
 
 def something
   x = Something.new
-  #Info: Lint_UselessSetterCall
   x.attr = 5
 end
