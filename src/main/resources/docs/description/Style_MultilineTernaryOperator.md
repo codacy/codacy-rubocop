5d96faf5275f8b227b9af4bbf62c7a968df7d0f5
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-09-03 17:35:53 +0100

    implements multiline ternary operator patterns

---
+++ src/main/resources/docs/description/Style_MultilineTernaryOperator.md
@@ -0,0 +1,4 @@
+##Multiline Ternary Operator
+
+Avoid using a ternary operator(```:?```) in multiple lines use if/unless instead
+
---
+++ src/main/resources/docs/tests/Style_MultilineTernaryOperator.rb
@@ -0,0 +1,5 @@
+##Patterns: Style_MultilineTernaryOperator
+
+##Info: Style_MultilineTernaryOperator
+if_this_is_a_true_value ?
+    then_the_result_is_this : else_it_is_this
