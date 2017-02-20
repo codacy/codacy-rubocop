66169601199c844d64af2cd001c43a39b4933997
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2017-02-20 15:11:16 +0000

    Update rubocop to version 0.47.1

---
+++ src/main/resources/docs/tests/Style_MethodCallWithArgsParentheses.rb
@@ -0,0 +1,8 @@
+##Patterns: Style_MethodCallWithArgsParentheses
+
+top.test
+
+top.test(a, b)
+
+##Info: Style_MethodCallWithArgsParentheses
+top.test a, b
---
+++ src/main/resources/docs/tests/Style_MethodCallWithoutArgsParentheses.rb
@@ -0,0 +1,8 @@
+##Patterns: Style_MethodCallWithoutArgsParentheses
+
+##Info: Style_MethodCallWithoutArgsParentheses
+top.test()
+
+Test()
+
+top.test
