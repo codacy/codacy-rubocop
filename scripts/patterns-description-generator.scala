3921222ac87653e1e5418e618eb064a84a40b8f7
Carlos Pinho <carlosdspinho@gmail.com>
2017-09-05 10:32:40 +0100

    Use all information in description.json
    The patterns and description generator script was changed and now it
    handles all the information.

--- scripts/patterns-description-generator.scala
+++ scripts/patterns-description-generator.scala
@@ -4,10 +4,26 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import play.api.libs.json._
 
 def updatePatternsAndDescription(version: String): Unit = {
-  def yamlReader: ObjectMapper = new ObjectMapper(new YAMLFactory()).configure(Feature.ALLOW_COMMENTS, true).configure(Feature.AUTO_CLOSE_SOURCE, true).configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true).configure(Feature.ALLOW_SINGLE_QUOTES, true).configure(Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true)
+  val filesName = Set("default", "disabled", "enabled")
 
-  val cops = scala.io.Source.fromFile(new java.io.File(s"rubocop-config-base-files/$version.yml")).getLines.toList.mkString("\n")
-  val json = Json.parse(yamlReader.readTree(cops).toString).as[JsObject]
+  val json = filesName.map { file =>
+    val cops = scala.io.Source.fromFile(new java.io.File(s"rubocop-config-base-files/$file.yml")).getLines.toList.mkString("\n")
+
+    def yamlReader: ObjectMapper = new ObjectMapper(new YAMLFactory()).configure(Feature.ALLOW_COMMENTS, true).configure(Feature.AUTO_CLOSE_SOURCE, true).configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true).configure(Feature.ALLOW_SINGLE_QUOTES, true).configure(Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true)
+
+    Json.parse(yamlReader.readTree(cops).toString).as[JsObject]
+  }.foldLeft(Json.obj()) { case (jsonAcc, json) => jsonAcc.deepMerge(json) }
+
+  val patterns = generatePatterns(json, version)
+  println(s"\n${Json.obj("name" -> "Rubocop", "version" -> version, "patterns" -> Json.parse(Json.toJson(patterns).toString).as[JsArray])}\n")
+
+  val descriptions = generateDescriptions(json, version)
+  println(s"\n${Json.parse(Json.toJson(descriptions).toString).as[JsArray]}\n")
+
+  println("NOTE: Copy the parameters description from the old description.json th ")
+}
+
+def generatePatterns(json: JsObject, version: String): JsArray = {
   val allPatterns = json.value.collect { case (pattern, JsObject(obj)) => (pattern.replace("/", "_"), obj.filterNot { case (key, value) => key.startsWith("Supported") || key.startsWith("StyleGuide") || key.startsWith("Enabled") || key.startsWith("Description") || key.startsWith("Reference") || key.startsWith("AutoCorrect") }) }
   val codacyPatterns = allPatterns.collect { case (pattern, parameterMap) =>
     val basePattern = Json.obj(
@@ -23,26 +39,19 @@ def updatePatternsAndDescription(version: String): Unit = {
       basePattern
     }
   }
+  Json.parse(Json.toJson(codacyPatterns).toString).as[JsArray]
+}
+
+def generateDescriptions(json: JsObject, ersion: String): JsArray = {
   val allPatternsWithDescs = json.value.collect { case (pattern, JsObject(obj)) => (pattern.replace("/", "_"), obj.filterNot { case (key, value) => key.startsWith("Supported") || key.startsWith("StyleGuide") || key.startsWith("Enabled") || key.startsWith("Reference") || key.startsWith("AutoCorrect") }) }
   val codacyPatternsDescs = allPatternsWithDescs.collect { case (pattern, parameterMap) if parameterMap.get("Description").isDefined =>
-    val filteredPatMap = parameterMap.filterNot { case (pattern, _) => pattern == "Description" }
     val basePattern = Json.obj(
       "patternId" -> pattern,
       "title" -> parameterMap.get("Description").get,
       "description" -> parameterMap.get("Description").get,
       "timeToFix" -> 5
     )
-    if (filteredPatMap.keys.nonEmpty) {
-      basePattern ++ Json.obj(
-        "parameters" -> Json.parse(Json.toJson(filteredPatMap.map { case (key, value) => Json.obj("name" -> key, "description" -> "<TODO: parameter description>") }).toString).as[JsArray]
-      )
-    } else {
-      basePattern
-    }
+    basePattern
   }
-  println(Json.obj("name" -> "Rubocop", "version" -> version, "patterns" -> Json.parse(Json.toJson(codacyPatterns).toString).as[JsArray]))
-  println("\n\n")
-  println(Json.parse(Json.toJson(codacyPatternsDescs).toString).as[JsArray])
-  println("\n\n")
-  println("NOTE: Look for '<TODO: parameter description>' on the descriptions .json, and fix them accordingly")
+  Json.parse(Json.toJson(codacyPatternsDescs).toString).as[JsArray]
 }
